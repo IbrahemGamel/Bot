86ca94f37a49ac6c1adc5c3f7851aabe15e1a71c
Blasome <52453521+IbrahemGamel@users.noreply.github.com>
2023-03-09 17:14:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 import os
 
 if __name__ == '__main__':
-  my_secret = 'MTA3MzQ1OTkyODg3MTczMTMyMA.GC9j8F.7kq6pOCe3RFDhc46Vt9eHx6hGTTothZ5tW0HPg'
+  my_secret = os.environ["DISCORD_TOKEN"]
   TOKEN = my_secret
   bot = MyClient
   bot.run_bot(discord.Client, TOKEN)
